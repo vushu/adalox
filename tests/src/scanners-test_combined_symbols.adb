1b83d41475f444b6c2d01aac06842f82a38cf197
davu <davu@mir-robots.com>
2024-03-09 23:22:54 +0100

    removed windows test

--- tests/src/scanners-test_combined_symbols.adb
+++ tests/src/scanners-test_combined_symbols.adb
@@ -1,6 +1,4 @@
 with AUnit.Assertions; use AUnit.Assertions;
-with Ada.Containers;   use Ada.Containers;
-with Ada.Text_IO;      use Ada.Text_IO;
 with Lexeme_Strings;   use Lexeme_Strings;
 package body Scanners.Test_Combined_Symbols is
    procedure Test_Double_Tokens is
@@ -9,8 +7,9 @@ package body Scanners.Test_Combined_Symbols is
    begin
       for I in 0 .. TokenList.Last_Index loop
          declare
-            T : Token           := TokenList (I);
-            C : String (1 .. 2) := Input_String (I + I + 1 .. I + I + 2);
+            T : constant Token           := TokenList (I);
+            C : constant String (1 .. 2) :=
+              Input_String (I + I + 1 .. I + I + 2);
          begin
             Assert (T.Lexeme = C, "Should match");
          end;
